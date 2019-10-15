6dbf27d2a1cd0ddc7f8d69a8046b2063ac5b6625
Tom <tom.vanschaijk@amadeus.com>
2019-10-15 13:12:06 +0200

    Added domain entities

---
+++ RomanianVioletRollsRoyce.Domain/Entities/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RomanianVioletRollsRoyce.Domain.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public ICollection<Account> Accounts { get; set; } = new List<Account>();
+    }
+}
